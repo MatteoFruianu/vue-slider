dc1c89e826e1e2b4a16b6248d519e938528acf38
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-09 16:01:01 +0100

    Arrow sliding

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,41 @@ const app = new Vue ({
 
 el: '#app',
 
+data: {
 
+    indexPhoto: 0,
+
+    photos: [
+      './images/image1.jpg',
+      './images/image2.jpg',
+      './images/image3.jpg',
+      './images/image4.jpg',
+    ]
+
+},
+
+methods: {
+
+  nextPhoto() {
+    this.indexPhoto += 1;
+
+    if (this.indexPhoto === this.photos.length - 1) {
+      this.indexPhoto = 0;
+    } 
+  },
+
+  prevPhoto() {
+    
+    this.indexPhoto -= 1;
+
+    if (this.indexPhoto < 0) {
+      this.indexPhoto = this.photos.length -1
+    }
+
+
+  }
+
+}
 
 
 })
