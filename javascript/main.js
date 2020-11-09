45da612506067aadb9759e4082db4d15462bcd11
Matteo Fruianu <matteofruianu@gmail.com>
2020-11-09 15:34:29 +0100

    Slider vue css

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+
+
+const app = new Vue ({
+
+el: '#app',
+
+
+
+
+})
